7e8be55096bdfc8b81cd2c671d0b7070f25d8226
Sergi Dote Teixidor <sergidt@gmail.com>
2021-04-05 12:29:15 +0200

    search

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,12 @@
-import { Exercise } from './exercise/main';
+import { arrayTest } from './array';
+import { queueTest } from './queue';
+import { stackTest } from './stack';
 
 console.clear();
 
-// AdapterTest.test();
-// BridgeTest.test();
-// SingletonTest.test();
+arrayTest();
 
-// PrototypeTest.test();
 
-// FactoryMethodTest.test();
-// BuilderTest.test();
-// AbstractFactoryTest.test();
-// FacadeTest.test();
+//stackTest();
 
-// VisitorTest.test();
-// IteratorTest.test();
-
-// ObserverTest.test();
-
-// MementoTest.test();
-
-// StateTest.test();
-
-// FlyweightTest.test();
-// StrategyTest.test();
-
-// CompositeTest.test();
-
-//MediatorTest.test();
-
-// DecoratorTest.test();
-// TemplateMethodTest.test();
-
-// InterpreterTest.test();
-
-// ChainOfResponsibilityTest.test();
-
-// HOME AUTOMATION SYSTEM TEST EXERCISE
-
-Exercise.execute();
-
+//queueTest();
