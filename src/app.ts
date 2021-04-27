801e172bdd132bb66775756cd237016f46937436
Sergi Dote Teixidor <sergidt@gmail.com>
2021-04-27 20:24:10 +0200

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { ratInAMazeTest } from './algorithms/backtracking/rat-in-a-maze';
 import { sudokuSolver } from './algorithms/backtracking/sudoku-solver';
 import { arrayTest } from './data-structures/array';
-import { dequeueTest } from './data-structures/deque';
+import { dequeueTest } from './data-structures/dequeue';
 import { doubleLinkedListTest } from './data-structures/double-linked-list';
 import { hastTableTest } from './data-structures/hast-table';
 import { linkedListTest } from './data-structures/linked-list';
@@ -9,7 +9,7 @@ import { queueTest } from './data-structures/queue';
 import { setTest } from './data-structures/set';
 import { stackTest } from './data-structures/stack';
 import { arrayExercises } from './exercises/array-exercises';
-import { dequeueExercises } from './exercises/deque-exercises';
+import { dequeueExercises } from './exercises/dequeue-exercises';
 import { queueExercises } from './exercises/queue-exercises';
 
 console.clear();
